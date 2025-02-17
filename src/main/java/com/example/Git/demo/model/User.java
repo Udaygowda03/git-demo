e56aeb4631eb7dd1871b0a3120a6463c42576155
Udaygowda03 <udaygowda962@gmail.com>
2025-02-17 15:32:17 +0530

    adding User Entity

---
+++ src/main/java/com/example/Git/demo/model/User.java
@@ -0,0 +1,40 @@
+package com.example.Git.demo.model;
+
+public class User {
+    private int userId;
+    private String UserName;
+    private String userEmail;
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
